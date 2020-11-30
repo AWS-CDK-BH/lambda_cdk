59a59f9c8afcf0a72b3d4361d1aff1a3e188ac2f
sam1610 <samir.amri@gmail.com>
2020-11-30 10:32:49 +0300

    update lamdba

--- bin/servelss-with-cdk.ts
+++ bin/servelss-with-cdk.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { ServelssWithCdkStack } from '../lib/servelss-with-cdk-stack';
 
 const app = new cdk.App();
-new ServelssWithCdkStack(app, 'ServelssWithCdkStack');
+new ServelssWithCdkStack(app, 'ServelssWithCdkStack', {
+    env:{
+        region: "us-east-1",
+        
+    }
+});
--- lib/servelss-with-cdk-stack.ts
+++ lib/servelss-with-cdk-stack.ts
@@ -1,9 +1,33 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+
 
 export class ServelssWithCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    //  dynamodb
 
-    // The code that defines your stack goes here
+    const table= new dynamodb.Table( this, "people", {
+      partitionKey: { name: 'name',type:dynamodb.AttributeType.STRING},
+      tableName :"StreamUsersTable"
+    })
+    //   lambda function 
+    const lmb= new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code : lambda.Code.fromAsset("lambda"),  //  folder name
+      handler : "hello.handler"
+
+    });
+
+    //  Api Gate Way 
+    const api= new apigw.RestApi( this, "StreamAPIGateway") ; 
+    //   lambda  integration 
+
+    const apIntegration= new apigw.LambdaIntegration(lmb);
+    const apiHello= api.root.addResource('hello'); // this for the  /hello  following the api Url
+    apiHello.addMethod("GET", apIntegration);
+
   }
 }
